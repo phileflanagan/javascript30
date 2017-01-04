09cb0975c6d38aa6d93c9387b731f3bdea141397
Phil Flanagan <phileflanagan@gmail.com>
2017-01-04 12:17:23 -0500

    refactor day 2

---
+++ Day 2 - Clock/index.js
@@ -0,0 +1,27 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  if (secondsDegrees === 90) {
+    secondHand.style.transition = 'none';
+  }
+  if (secondsDegrees === 96) {
+    secondHand.style.transition = 'all 70ms';
+  }
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+int = setInterval(setDate, 1000);
