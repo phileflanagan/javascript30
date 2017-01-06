6181b83f66065defbb13d51aabcca29c581f3c50
Phil Flanagan <phileflanagan@gmail.com>
2017-01-06 14:17:26 -0500

    Finish solution 2

--- Day 4 - Array Cardio/index.js
+++ Day 4 - Array Cardio/index.js
@@ -23,10 +23,14 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
 const fifteen = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600);
+console.log('1. Born in 1500s');
 console.table(fifteen);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors' first and last names
+const fullNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+console.log('2. Full Names');
+console.log(fullNames);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
