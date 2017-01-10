5e9b4e9162a1af36f02feb7b76048a63b1b3cea3
Phil Flanagan <phileflanagan@gmail.com>
2017-01-10 18:35:43 -0500

    Finish drawing implementation

--- Day 8 - Canvas Fun/index.js
+++ Day 8 - Canvas Fun/index.js
@@ -2,18 +2,29 @@ const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.strokeSyle = '#bada55';
+ctx.strokeStyle="#bada55";
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 100;
 
 let isDrawing = false,
     lastX = 0,
     lastY = 0;
 
-// console.log(isDrawing, lastX, lastY)
-
 function draw(e) {
-  console.log(e);
+  if(!isDrawing) return;
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+
+  [lastX, lastY] = [e.offsetX, e.offsetY];
 }
 
 canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mouseup',   () => isDrawing = false);
+canvas.addEventListener('mouseout',  () => isDrawing = false);
