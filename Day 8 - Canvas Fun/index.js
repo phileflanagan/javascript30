c4087315ee5f32289032267d126900d36375d4bf
Phil Flanagan <phileflanagan@gmail.com>
2017-01-10 19:11:02 -0500

    Hipster-ify

--- Day 8 - Canvas Fun/index.js
+++ Day 8 - Canvas Fun/index.js
@@ -1,24 +1,38 @@
+const header = document.querySelector('.top-header');
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = document.body.clientWidth - 30;
+canvas.height = document.body.scrollHeight - header.clientHeight - 20;
 ctx.strokeStyle="#bada55";
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 100;
+ctx.globalCompositeOperation = 'xor'
 
 let isDrawing = false,
-    lastX = 0,
-    lastY = 0;
+    direction = true,
+    lastX     = 0,
+    lastY     = 0
+    hue       = 0;
 
 function draw(e) {
   if(!isDrawing) return;
+
+  ctx.strokeStyle = `hsl(${hue}, 40%, 60%)`;
+
   ctx.beginPath();
   ctx.moveTo(lastX, lastY);
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
 
   [lastX, lastY] = [e.offsetX, e.offsetY];
+
+  hue += 2;
+  if(hue >=360) hue = 0;
+
+  if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1) direction = !direction;
+  if(direction)  ctx.lineWidth++;
+  if(!direction) ctx.lineWidth--;
 }
 
 canvas.addEventListener('mousemove', draw);
