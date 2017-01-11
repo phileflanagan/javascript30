38a2c7b3e8527def0a1cfb8f0e7160c252d05b1f
Phil Flanagan <phileflanagan@gmail.com>
2017-01-10 21:59:50 -0500

    Implement all of day 9

--- Day 9 - Dev Tool Tricks/index.js
+++ Day 9 - Dev Tool Tricks/index.js
@@ -1,32 +1,56 @@
 
-  const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
+const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
 
-  function makeGreen() {
-    const p = document.querySelector('p');
-    p.style.color = '#BADA55';
-    p.style.fontSize = '50px';
-  }
+function makeGreen() {
+  const p = document.querySelector('p');
+  p.style.color = '#BADA55';
+  p.style.fontSize = '50px';
+}
 
-  // Regular
+// Regular
+console.log('Hello')
 
-  // Interpolated
+// Interpolated
+const iv = 'interpolated value'
+console.log('Hello I am a old-school %s', iv)
+console.log(`Hello I am an ES6 ${iv}`)
 
-  // Styled
+// Styled
+console.log('%c Hello', 'font-size: 2rem; color: #BADA55; text-shadow: 1px 1px 2px green')
 
-  // warning!
+// warning!
+console.warn('some warning')
 
-  // Error :|
+// Error :|
+console.error('ehh whatcha gonna do')
 
-  // Info
+// Info
+console.info('male platapi are poisonous... or is it venomous')
 
-  // Testing
+// Testing
+console.assert(1 === 2, 'that\'s where you\'re wrong kiddo')
 
-  // clearing
+// clearing
+// console.clear()
 
-  // Viewing DOM Elements
+// Viewing DOM Elements
+const p = document.querySelector('p')
+console.log(p)
+console.dir(p)
 
-  // Grouping together
+// Grouping together
+dogs.forEach(dog => {
+  console.group(`${dog.name}`)
+  console.log(`This is ${dog.name}`)
+  console.log(`${dog.name} is ${dog.age} years old`)
+  console.log(`That is ${dog.age * 7} in dog years`)
+  console.groupEnd(`${dog.name}`)
+})
 
-  // counting
+// counting
+console.count('joe')
+console.count('joe')
 
-  // timing
+// timing
+console.time('fetching data')
+setTimeout(() => console.timeEnd('fetching data'), 1000);
