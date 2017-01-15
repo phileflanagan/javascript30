ee961fe83f4b0c7445bbd8d0f9c81d3d3bea581b
Phil Flanagan <phileflanagan@gmail.com>
2017-01-15 15:30:20 -0500

    Finish implementation of day 13

--- Day 13 - Slide in on Scroll/index.js
+++ Day 13 - Slide in on Scroll/index.js
@@ -1,4 +1,4 @@
-function debounce(func, wait = 20, immediate = true) {
+function debounce(func, wait = 10, immediate = true) {
   var timeout;
   return function() {
     var context = this, args = arguments;
@@ -12,3 +12,26 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+  sliderImages.forEach(sliderImage => {
+    // half image exposed
+    const slideInAt = (window.scrollY + window.innerHeight) - (sliderImage.height / 2);
+
+    // bottom of image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add('active');
+    } else {
+      sliderImage.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
