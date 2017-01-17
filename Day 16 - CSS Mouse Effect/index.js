989d731a551b29ec3e67cec1949810e31685aad3
Phil Flanagan <phileflanagan@gmail.com>
2017-01-17 17:47:47 -0500

    Add calculations for later implementation

--- Day 16 - CSS Mouse Effect/index.js
+++ Day 16 - CSS Mouse Effect/index.js
@@ -3,13 +3,22 @@ const text = hero.querySelector('h1');
 const walk = 100; // 100px;
 
 function shadow(e) {
-  const { offsetWidth: width, offsetHeight: height } = this;
+  const { offsetWidth: width, offsetHeight: height, offsetLeft: left, offsetTop: top } = this;
   let { offsetX: x, offsetY: y } = e;
 
   if (this !== e.target) {
     x += e.target.offsetLeft;
     y += e.target.offsetTop;
   }
+
+  // console.dir(this);
+  const xCenter = ( left + width) / 2;
+  const yCenter = ( top + height) / 2;
+  console.log(e.pageX);
+  console.log(this.offsetLeft);
+  const angle = Math.atan2((e.pageX - xCenter), -(e.pageY - yCenter)) * (180/Math.PI);
+
+  // console.log(angle);
 
   const xWalk = Math.round((x / width * walk) - (walk / 2));
   const yWalk = Math.round((y / height * walk) - (walk / 2));
